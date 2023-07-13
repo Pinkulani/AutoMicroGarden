ce9dc7d0220f56b5365e42e6be3a1be9f8870673
Emilia <97615383+Pinkulani@users.noreply.github.com>
2023-07-13 19:55:10 +0200

    Add files via upload

---
+++ AutoMicroGarden/StartUp.ino
@@ -0,0 +1,9 @@
+void StartUp(void) {
+  Timer = 100; // LED will turn on for 10 Seconds when loading the program
+  while (Timer > 1) {
+    digitalWrite(LED, HIGH);
+    delay(100);
+    Timer --;
+  }
+  digitalWrite(LED, LOW);
+}
